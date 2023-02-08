e239593d6911f7837b5a924a7f42feb2dd471cf2
Brian Walvoord <brian.walvoord@gmail.com>
2023-02-08 16:13:50 +0900

    chore: removed tech section

--- index.js
+++ index.js
@@ -19,19 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// const appear = document.querySelector(".appear");
-// const cb = function (entries) {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("inview");
-//     } else {
-//       entry.target.classList.remove("inview");
-//     }
-//   });
-// };
-// const io = new IntersectionObserver(cb);
-// io.observe(appear);
-
 const items = document.querySelectorAll(".appear");
 
 const active = function (entries) {
